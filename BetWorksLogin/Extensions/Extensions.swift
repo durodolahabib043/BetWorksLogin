1a1e50eedd5e9220c983effb191c6005d90c021f
habib <durodolahabib043@gmail.com>
2020-12-24 17:17:38 -0500

    added delegates to textfield

--- BetWorksLogin/Extensions/Extensions.swift
+++ BetWorksLogin/Extensions/Extensions.swift
@@ -20,11 +20,18 @@ extension LoginViewController {
         fullStackView.translatesAutoresizingMaskIntoConstraints = false
 
         view.addSubview(fullStackView)
-        //centerY
+
+        //MARK:- headerText
+        constraints += [NSLayoutConstraint.init(item: headerText, attribute: .bottom, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .top, multiplier: 1.0, constant: 100.0)]
+        constraints += [NSLayoutConstraint.init(item: headerText, attribute: .leading, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .leading, multiplier: 1.0, constant: 40.0)]
+        constraints += [NSLayoutConstraint.init(item: headerText, attribute: .trailing, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .trailing, multiplier: 1.0, constant: -40.0)]
+
+        //MARK:- fullStackView
+        //centerY fullStackView
         constraints += [NSLayoutConstraint.init(item: fullStackView, attribute: .centerY, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .centerY, multiplier: 1.0, constant: 0.0)]
-        // leading
+        // leading fullStackView
         constraints += [NSLayoutConstraint.init(item: fullStackView, attribute: .leading, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .leading, multiplier: 1.0, constant: 40.0)]
-        // trailing
+        // trailing fullStackView
         constraints += [NSLayoutConstraint.init(item: fullStackView, attribute: .trailing, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .trailing, multiplier: 1.0, constant: -40.0)]
         // height
         constraints += [NSLayoutConstraint.init(item: userNameTextField, attribute: .height, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .height, multiplier: 1/12, constant: 0.0)]
@@ -34,3 +41,40 @@ extension LoginViewController {
         view.addConstraints(constraints)
     }
 }
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+
+extension LoginViewController {
+
+    @objc func keyboardWillShow(notification: Notification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            print("notification: Keyboard will show")
+            if self.view.frame.origin.y == 0{
+                self.view.frame.origin.y -= keyboardSize.height - 250
+            }
+        }
+
+    }
+
+    @objc func keyboardWillHide(notification: Notification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y != 0 {
+                self.view.frame.origin.y += keyboardSize.height - 250
+            }
+        }
+    }
+
+}
+
+
--- BetWorksLogin/Views/LoginViewController.swift
+++ BetWorksLogin/Views/LoginViewController.swift
@@ -3,19 +3,28 @@
 //  BetWorksLogin
 //
 //  Created by Habib Durodola on 2020-12-24.
-//
+
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController , UITextFieldDelegate{
 
     //MARK:- VIEWS
+
+    let headerText: UILabel = {
+        let label = UILabel()
+        label.text = "Welcome to FireFly!"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        return label
+    }()
     lazy var userNameTextField: UITextField = {
         let textfield = UITextField()
         textfield.translatesAutoresizingMaskIntoConstraints = false
         textfield.borderStyle = .line
         textfield.placeholder = "UserName "
         textfield.backgroundColor = .lightGray
+        textfield.delegate = self
         return textfield
     }()
 
@@ -25,6 +34,7 @@ class LoginViewController: UIViewController {
         textField.borderStyle = .line
         textField.placeholder = "Password "
         textField.backgroundColor = .lightGray
+        textField.delegate = self
         return textField
     }()
 
@@ -46,12 +56,17 @@ class LoginViewController: UIViewController {
         setupInvisibleNavigation()
         setupViews()
         setupAutoLayout()
+        hideKeyboardWhenTappedAround()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+
         // Do any additional setup after loading the view.
     }
 
     //MARK:- HANDLERS
 
     fileprivate func setupViews(){
+        view.addSubview(headerText)
         view.addSubview(userNameTextField)
         view.addSubview(passwordTextField)
         view.addSubview(loginBtn)
@@ -61,6 +76,13 @@ class LoginViewController: UIViewController {
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
         self.navigationController?.navigationBar.isTranslucent = true
+    }
+
+    //MARK:- DELEGATES
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        userNameTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        return true
     }
 
 }
